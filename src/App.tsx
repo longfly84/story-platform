b4a1f06a118016f39d183f3cd6647088251d95f6
LongFly84 <longfly84@gmail.com>
2026-04-30 18:56:50 +0700

    Fix 404 routing and improve app navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
+import { Routes } from "react-router"
 import HomePage from "@/pages/HomePage"
 import ReaderPage from "@/pages/ReaderPage"
 import StoryDetailPage from "@/pages/StoryDetailPage"
+import NotFoundPage from "@/pages/NotFoundPage"
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/truyen/:slug" element={<StoryDetailPage />} />
-        <Route path="/doc-truyen/:slug/:chapter" element={<ReaderPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/truyen/:slug" element={<StoryDetailPage />} />
+          <Route path="/doc-truyen/:slug/:chapter" element={<ReaderPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
     </BrowserRouter>
   )
 }
